ce2cf6a5182c9bf3cf8027166940b78960565730
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-09-15 21:12:05 -0700

    accounts model file completed

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,38 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+  const accounts = await db("accounts");
+  return accounts;
+};
 
-const getById = id => {
-  // DO YOUR MAGIC
-}
+const getById = (id) => {
+  const account = await db("accounts").where("id", id).first();
+  return account;
+};
 
-const create = account => {
-  // DO YOUR MAGIC
-}
+const create = (account) => {
+  const [id] = await db("accounts").insert({
+    name: account.name.trim(),
+    budget: account.budget,
+  });
+  const newAccount = await getById(id);
+  return newAccount;
+};
 
 const updateById = (id, account) => {
-  // DO YOUR MAGIC
-}
+  await db("accounts")
+  .where("id", id)
+  .update(account);
+  return getById(id);
+};
 
-const deleteById = id => {
-  // DO YOUR MAGIC
-}
+const deleteById = (id) => {
+  const deletedAccount = await getById(id)
+  await db('accounts')
+  .where({ id })
+  .del()
+  return deletedAccount
+};
 
 module.exports = {
   getAll,
@@ -24,4 +40,4 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+};
